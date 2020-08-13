3f9c7c2e28f31accbeedf335935a6e862a32b6f4
Tiffani Best <tiffanimelissabest@gmail.com>
2020-08-13 00:00:00 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Mr. Chocolate', 20, 'M')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Rowdy', 10, 'M')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Tabitha', 6, 'F')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Sergeant Brown', 19, 'M')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Melissa', 13, 'F')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Grinch', 2, 'M')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('Wendy', 6, 'F')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES('NULL', 20, 'F')
